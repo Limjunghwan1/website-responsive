cb22caf59261ce8c14d76d53f2100b9c840cd652
Limjunghwan1 <q7mnb99@naver.com>
2023-09-13 16:11:49 +0900

    0913 반응형웹

---
+++ js/index.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+
+
+	//모바일용 메뉴버튼________________
+	$(".mo_view").css({"right":"-60%"}); //처음위치
+	$(".close").hide();
+
+
+	$(".mo_menu_btn").click(function (){
+		$(".mo_view").stop(true, true).animate({"right":"0%"});
+		$(".close").show();
+	});
+
+	$(".close").click(function (){
+		$(".mo_view").stop(true, true).animate({"right":"-60%"});
+		$(".close").hide();
+	});
+
+
+
+
+
+});
